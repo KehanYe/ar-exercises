f4903f5c196916043b330821e6e35377945380d4
KehanYe <kehanfu@gmail.com>
2022-05-06 09:47:45 -0600

    exercise 6 - add employees, 1-to-many relationship

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,18 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Epictetus", last_name: "The Stoic", hourly_rate: 11 )
+@store1.employees.create(first_name: "Marcus", last_name: "Aurelius", hourly_rate: 350)
+@store1.employees.create(first_name: "Aristotle", last_name: "the Polymath", hourly_rate: 121)
+@store1.employees.create(first_name: "Dante", last_name: "Alighieri ", hourly_rate: 66)
+
+@store2.employees.create(first_name: "John", last_name: "Doe", hourly_rate: 45)
+@store2.employees.create(first_name: "John", last_name: "Dane", hourly_rate: 45)
+@store2.employees.create(first_name: "John", last_name: "Dale", hourly_rate: 45)
+@store2.employees.create(first_name: "John", last_name: "Dack", hourly_rate: 45)
+
+total_employees = Employee.count
+
+puts "Number Employees at Store 1: #{@store1.employees.count}"
+puts "Number Employees at Store 1: #{@store2.employees.count}"
+puts "Number Employees at all Stores: #{total_employees}"
